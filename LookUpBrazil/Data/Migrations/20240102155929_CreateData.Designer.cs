7f08150a2f76cb379a1914ed2cc501280fc5a9be
Leonardo Romeiro <leozinwork@gmail.com>
2024-01-10 13:03:36 -0300

    Bug

--- LookUpBrazil/Data/Migrations/20240102155929_CreateData.Designer.cs
+++ LookUpBrazil/Data/Migrations/20240102155929_CreateData.Designer.cs
@@ -4,6 +4,7 @@ using LookUpBrazil.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LookUpBrazil.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240102155929_CreateData")]
+    partial class CreateData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
