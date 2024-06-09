61382f715b2661119e84d6f9a4e32887f467a3dd
Leonardo Romeiro <leozinwork@gmail.com>
2024-06-09 14:40:24 -0300

    Fix: Correção de bug

--- LookUpBrazil.Test/Handler/GameTest.cs
+++ LookUpBrazil.Test/Handler/GameTest.cs
@@ -1,5 +1,6 @@
 ﻿using LookUpBrazil.Core.ObjectValues;
 using LookUpBrazil.Core.Requests.City;
+using LookUpBrazil.Core.Requests.Game;
 using LookUpBrazil.Core.Responses;
 using LookUpBrazil.Test.Repositories;
 using System;
@@ -13,6 +14,34 @@ namespace LookUpBrazil.Test.Handler
     public class GameTest
     {
         public FakeCityRepository CityRepository = new FakeCityRepository();
+        public FakeGameRepository GameRepository = new FakeGameRepository();
+        [TestMethod]
+        public async void DadoUmaTentativaCorretaDeveRetornarSucesso()
+        {
+            var game = await GameRepository.GetGameById(Guid.NewGuid());
+            var request = new AttemptRequest
+            {
+                GameId = game.Id,
+                Name = new Name("Maringa")
+            };
+
+            var cities = CityRepository.GetCitiesByLetter(game.Requirement.InitialLetter.Text);
+
+            var city = CityRepository.CityExists(request.Name.TextCompleted);
+
+            Response<City?> response;
+
+            if (city is null)
+            {
+                response = new Response<City?>(null, 300, "Cidade nao e valida");
+            }
+            else
+            {
+                response = new Response<City?>(city);
+            }
+
+            Assert.IsTrue(response.IsSuccess);
+        }
         [TestMethod]
         public void DadoUmCityValidoDeveRetornarSucesso()
         {
---
+++ LookUpBrazil.Test/Repositories/FakeGameRepository.cs
@@ -0,0 +1,25 @@
+﻿using LookUpBrazil.Core.Entities;
+using LookUpBrazil.Core.ObjectValues;
+using LookUpBrazil.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookUpBrazil.Test.Repositories
+{
+    public class FakeGameRepository : IGameRepository
+    {
+        public async Task<Game?> GetGameById(Guid id)
+        {
+            Game game;
+            do
+            {
+                game = new Game();
+            } while (game.Requirement.InitialLetter == new Letter('M'));
+
+            return game;
+        }
+    }
+}
