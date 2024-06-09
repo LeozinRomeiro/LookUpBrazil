7cae46ac62625c6949c4b03cdfcb3168aebadafc
Leonardo Romeiro <leozinwork@gmail.com>
2024-06-09 13:03:58 -0300

    Feat: Classe letra e expetion

---
+++ LookUpBrazil.Core/Exceptions/InvalidLetterException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LookUpBrazil.Core.Exceptions
+{
+    public partial class InvalidLetterException : Exception
+    {
+        private const string DefaultErrorMessage = "Letra invalida";
+
+        public InvalidLetterException(string message = DefaultErrorMessage)
+            : base(message)
+        {
+        }
+
+        public static void ThrowIfInvalid(
+            char letter,
+            string message = DefaultErrorMessage)
+        {
+            if (string.IsNullOrEmpty(letter.ToString()))
+                throw new InvalidLetterException(message);
+
+            if (!UrlRegex().IsMatch(letter.ToString()))
+                throw new InvalidLetterException("Letra incompativel com o formato definido");
+        }
+
+        [GeneratedRegex(
+            "^[A-Z]+$")]
+        private static partial Regex UrlRegex();
+    }   
+}
---
+++ LookUpBrazil.Test/ObjectValues/LetterTest.cs
@@ -0,0 +1,32 @@
+﻿using LookUpBrazil.Core.ObjectValues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookUpBrazil.Test.ObjectValues
+{
+    [TestClass]
+    public class LetterTest
+    {
+        [TestMethod]
+        [DataRow('A',true)]
+        [DataRow('Z',true)]
+        [DataRow('1',false)]
+        [DataRow('-',false)]
+        [DataRow('$',false)]
+        public void TestarAValidacaoDeLetras(char texto, bool isValid)
+        {
+            try
+            {
+                new Letter(texto);
+                Assert.IsTrue(isValid);
+            }
+            catch
+            {
+                Assert.IsFalse(isValid);
+            }
+        }
+    }
+}
