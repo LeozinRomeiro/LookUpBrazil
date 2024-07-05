ba8a72f689b4cbd8870e421b86142e342bce7d8d
Leonardo Romeiro <leozinwork@gmail.com>
2024-07-05 13:37:34 -0300

    feat: teste de construtor de Text

---
+++ LookUpBrazil.Test/ObjectValues/TextTest.cs
@@ -0,0 +1,20 @@
+﻿using LookUpBrazil.Core.ObjectValues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookUpBrazil.Test.ObjectValues
+{
+    [TestClass]
+    public class TextTest
+    {
+        [TestMethod]
+        public void ConstrutorDeveRemoverOsAcentosDaLetraInicial()
+        {
+            var text = new Text("Ácido");
+            Assert.AreEqual(text.InitialLetter.Character, 'A');
+        }
+    }
+}
